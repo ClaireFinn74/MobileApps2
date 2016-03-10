18295b3baa9a9e8e04db4c38bf0fefaeb68ce6ad
Claire Finn <Clairefinn74@yahoo.ie>
2016-03-10 16:58:49 +0000

    Commit7
    Added images to the Play page where the game will take place, added a pop-up box with instructions on how to play the game

---
+++ Play.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WorldOfWool
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Play : Page
+    {
+        public Play()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnBack_click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private async void btnInstructions_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new MessageDialog("So, trouble sleeping? \n Try to tap as many sheep as you possibly can within the time limit! \n That should get you right to sleep! \n Good Luck!!");
+            await dialog.ShowAsync();
+        }
+    }
+}
